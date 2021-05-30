7107368bbfea9344effd8e25034f0be974e46c9d
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2021-05-30 13:19:36 +0530

    Created HackerRank C++ Solutions

---
+++ Introduction/Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+     int a;
+     long b;
+     char c;
+     float d;
+     double e;
+     cin>>a>>b>>c>>d>>e;
+     cout<<a<<endl<<b<<endl<<c<<endl<<fixed<<setprecision(3)<<d<<fixed<<setprecision(9)<<endl<<e;
+    return 0;
+}
+
